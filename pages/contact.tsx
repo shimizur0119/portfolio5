2b660101070c795fd0ca7f60cc74f67a74da195d
Ryota Shimizu <shimizu_ryota@giginc.co.jp>
2023-06-17 14:04:15 +0900

    add: contact page

--- pages/contact.tsx
+++ pages/contact.tsx
@@ -2,6 +2,8 @@ import CommonFvSection from "../components/CommonFvSection";
 import CommonHead from "../components/CommonHead";
 import Footer from "../components/Footer";
 
+import s from "./contact.module.scss";
+
 export default function Contact() {
   return (
     <>
@@ -12,6 +14,21 @@ export default function Contact() {
       <div className="pageWrap">
         <main>
           <CommonFvSection desc="お問い合わせはこちら" title="Contact" />
+          <div className={s.contentSection}>
+            <div className={s.inner}>
+              <div className={s.content}>
+                こちらのGoogle Formからお問い合わせください。
+                <br />
+                <a
+                  href="https://forms.gle/Qsnceu3k9EC8q4NN6"
+                  rel="noopener noreferrer"
+                  target="_blank"
+                >
+                  https://forms.gle/Qsnceu3k9EC8q4NN6
+                </a>
+              </div>
+            </div>
+          </div>
         </main>
         <Footer />
       </div>
